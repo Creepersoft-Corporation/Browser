e16e2b5525ceb86178b4e8d6e3818efe92468752
吉田 保貴 <yasutaka.y@kusacityedu.onmicrosoft.com>
2021-05-04 11:00:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Browser Test 1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Browser_Test_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.texturl = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.wB1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(993, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.texturl);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(166, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(5);
+            this.panel3.Size = new System.Drawing.Size(827, 42);
+            this.panel3.TabIndex = 3;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // texturl
+            // 
+            this.texturl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.texturl.Location = new System.Drawing.Point(5, 5);
+            this.texturl.Name = "texturl";
+            this.texturl.Size = new System.Drawing.Size(817, 19);
+            this.texturl.TabIndex = 3;
+            this.texturl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.texturl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.texturl_KeyDown);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.btnBack);
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.btnHome);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 42);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 9);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(43, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "戻る";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(61, 9);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(43, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "進む";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(110, 9);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(43, 23);
+            this.btnHome.TabIndex = 2;
+            this.btnHome.Text = "ホーム";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // wB1
+            // 
+            this.wB1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wB1.Location = new System.Drawing.Point(0, 42);
+            this.wB1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wB1.Name = "wB1";
+            this.wB1.Size = new System.Drawing.Size(993, 653);
+            this.wB1.TabIndex = 2;
+            this.wB1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wB1_DocumentCompleted);
+            this.wB1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.wB1_Navigated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 695);
+            this.Controls.Add(this.wB1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox texturl;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.WebBrowser wB1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Browser Test 1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser_Test_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            wB1.Url = new Uri("http://www.google.co.jp");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        //ホーム
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            wB1.Url = new Uri("http://www.google.co.jp");
+        }
+
+        //戻る
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            wB1.GoBack();
+        }
+
+        //進む
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            wB1.GoForward();
+        }
+
+        private void wB1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        //ページが移動した後のイベント
+        private void wB1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            texturl.Text = wB1.Url.ToString();
+
+            btnBack.Enabled = wB1.CanGoBack;
+            btnNext.Enabled = wB1.CanGoForward;
+
+        }
+
+        private void texturl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter)) {
+                wB1.Url = new Uri(texturl.Text);
+            }
+        }
+    }
+}
